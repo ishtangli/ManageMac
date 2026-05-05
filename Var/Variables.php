6df9ffc94e2f7fc33e628a5da4ac51bc95dfe7e1
ishtangli <ishtangli@gmail.com>
2026-05-05 08:42:00 +0800

    Add files via upload

---
+++ Var/Variables.php
@@ -0,0 +1,13 @@
+<?php
+	function GetDB() {
+		return "A380";
+	}
+
+	function GetSessionVar() {
+		return "A380PROD";
+	}
+
+	function GetTitle() {
+		return "Manage Mac 3.1 - A380 Production";
+	}
+?>
